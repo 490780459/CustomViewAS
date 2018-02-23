81691a7847a49015e4740f1bb1d3900fba31289e
liupeiyang <liupeiyang@hongte.info>
2018-02-23 18:17:26 +0800

    加入自定义控件

---
+++ app/src/main/java/junte/customview/views/CustomView1.java
@@ -0,0 +1,16 @@
+package junte.customview.views;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by liupeiyang on 2018/2/23.
+ */
+
+public class CustomView1 extends View {
+    public CustomView1(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
